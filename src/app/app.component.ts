f856d5945744075cfa6c7390395a037fbe17bdda
Rudy Morosoff <rudy.morosoff@libertymutual.com>
2017-09-25 12:56:43 -0700

    Included validation logic in the withModel branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms'
 
 @Component({
@@ -9,6 +9,9 @@ import { NgForm } from '@angular/forms'
 export class AppComponent {
   title = 'app';
 
+  sampleForm: NgForm;
+  @ViewChild('sampleForm') currentForm: NgForm;
+
 
   model: object = {
     first_name: "Bob",
@@ -18,4 +21,51 @@ export class AppComponent {
   onSubmit(){
     console.log(this.model)
   }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.sampleForm) { return; }
+    //set the form to the current form for comparison
+    this.sampleForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.sampleForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    let form = this.sampleForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an emtpy string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  };
+
+  validationMessages = {
+    'first_name': {
+      'required':      'First name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'last_name': {
+      'minlength':     'Name must be at least 2 characters long.'
+    }
+  };
 }
